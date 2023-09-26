104b22bab855af6ad83b97343b185cee41a8cc56
Thore Brehmer <thoreservice@icloud.com>
2023-09-26 20:03:20 +0200

    add: anonymousFunctinos.swift

--- AnonymousFunctions.swift
+++ AnonymousFunctions.swift
@@ -6,3 +6,26 @@
 //
 
 import Foundation
+
+///----------ANONYMOUS FUNCTIONS
+/// (similar to dynamic variables, but for anonymous function)
+/// ///anonymous funcs are pretty useless -> closure syntax is important
+struct AnonymousFunctions{
+    //without paras and implicit return
+    var closure = {
+        print("hey")
+    }
+    closure() //is called like a function
+    
+    //without para but deklared return
+    var ret: () -> Int = {
+        return 5
+    }
+    let _: Int = ret()
+    
+    //with paras and declared return
+    var ver: (String) -> Void = { input in
+        //do smth
+    }
+    ver("")
+}
