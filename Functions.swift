7589fb835fec34c5a4744ae4acea33dd71be8ea0
Thore Brehmer <thoreservice@icloud.com>
2023-09-26 20:02:44 +0200

    add: functions.swift

--- Functions.swift
+++ Functions.swift
@@ -6,3 +6,51 @@
 //
 
 import Foundation
+
+///----------FUNCTIONS
+struct functions{
+    //ohne rückgabe
+    func voidFunc(input: String){
+        print(input)
+    }
+    
+    //mit rückgabe
+    func intCallback(a: Int, b: Int) -> Int {
+        return a+b
+    }
+    
+    //anonymous + extern/intern + default parameters
+    func test(_ a: Int, extern intern: Int, defaultV: Int = 0) {
+        print (a + intern + defaultV)
+    }
+    test(1, extern: 2)
+    
+    //mit function als eingabe parameter
+    func functionalPrg(f: (String) -> String) -> String {
+        //strips return of f
+        return f("do smth").trimmed()
+    }
+    
+    
+    //named tupel rückgabe
+    func retMultiValues1() -> (name: String, age: Int, isStudent: Bool){
+        //1. option
+        //return ("Thore", 26, true)
+        //2. option
+        return (name : "Thore", age : 26, isStudent : true)
+    }
+    
+    //unnamed tuper rückgabe
+    func retMulitValues2() -> (String, Int, Bool){
+        return ("Thore", 26, true)
+    }
+    
+    //get values from tupel func
+    //1. option
+    let (name, age, _) = retMultiValues1()
+    //2. option
+    let user = retMultiValues1()
+    //print
+    print(name, age, user.isStudent)
+    print(user.0, user.age, user.2)
+}
