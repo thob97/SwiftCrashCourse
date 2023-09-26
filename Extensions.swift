5d6ee763b3f9b49c4512c30965bd25d68c41213a
Thore Brehmer <thoreservice@icloud.com>
2023-09-26 20:04:37 +0200

    add: extensions.swift

--- Extensions.swift
+++ Extensions.swift
@@ -6,3 +6,34 @@
 //
 
 import Foundation
+
+///----------EXTENSIONS (datenstrukturen/klassen/structs erweitern)
+//example 1: on datatype String
+extension String {
+    //example: for a shorter version of trimmingCharacters
+    func trimmed () -> String {
+        return self.trimmingCharacters(in: .whitespacesAndNewlines)
+    }
+    
+    //for inplace (instead of retuning a string) -> needs mutating
+    mutating func trim(){
+        self = self.trimmingCharacters(in: .whitespacesAndNewlines)
+    }
+    
+    //example: new dynamic variable
+    var lines: [String] {
+        self.components(separatedBy: .newlines)
+    }
+}
+
+//example 2: on protocoll (for lists, strings, dics)
+extension Collection {
+    var isNotEmpty: Bool {
+        return !isEmpty
+    }
+}
+func test(){
+    [1].isNotEmpty
+    "test".isNotEmpty
+    ["key":"value"].isNotEmpty
+}
