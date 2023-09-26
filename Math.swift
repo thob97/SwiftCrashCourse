74f7caabdd32aac7922c84f050b3c987c29e2e2d
Thore Brehmer <thoreservice@icloud.com>
2023-09-26 20:00:44 +0200

    add: math.swift

--- Math.swift
+++ Math.swift
@@ -6,3 +6,22 @@
 //
 
 import Foundation
+
+///----------MATH
+struct math{
+    //mostly useless types
+    let _: Int8 = Int8(0)
+    let _: Int16 = Int16(0)
+    let _: UInt8 = 0b00110011
+    let _: UInt16 = UInt16(0)
+    
+    //xor
+    let _ = 10 ^ 1
+    //and
+    let _ = 1 & 2
+    //or
+    let d = 1 | 152
+    //shift
+    let _ = d << 3
+    let _ = d >> 20
+}
