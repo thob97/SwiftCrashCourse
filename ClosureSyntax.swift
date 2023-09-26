149bde661578b16e9c4eb69eae5c43ba7c194c41
Thore Brehmer <thoreservice@icloud.com>
2023-09-26 20:03:03 +0200

    add: closureSyntax.swift

--- ClosureSyntax.swift
+++ ClosureSyntax.swift
@@ -6,3 +6,22 @@
 //
 
 import Foundation
+
+///----------CLOSURE SYNTAX
+struct closureSyntax{
+    let list = [0,1,2,3,4]
+    //longest versions
+    let longestVerison = list.filter({ (ele) -> Bool in
+        return ele == 1
+    })
+    //type inference
+    let mediumVerison = list.filter({ ele in
+        ele == 1
+    })
+    //trailing closure syntax
+    let trailingClosure = list.filter{ ele in
+        ele == 1
+    }
+    //with closure arguments
+    let shortestVersion = list.filter{$0 == 1}
+}
