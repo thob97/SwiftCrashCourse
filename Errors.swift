2c1791ccb9dedc14f75a1fbc2aedf5d283e1b36f
Thore Brehmer <thoreservice@icloud.com>
2023-09-26 20:02:51 +0200

    add: errors.swift

--- Errors.swift
+++ Errors.swift
@@ -6,3 +6,33 @@
 //
 
 import Foundation
+
+///----------ERRORS
+struct errors{
+    //create a custom error, has to implement swift standard Error
+    enum CustomError: Error{
+        case custom1, custom2
+    }
+    
+    //example func which throws customError
+    func checkSmth() throws -> String {
+        let rand: Int = Int.random(in: 0...100)
+        
+        if rand < 50{
+            throw CustomError.custom1
+        }
+        else{
+            return "Success"
+        }
+    }
+    
+    //(do) try catch clausle (needs the do)
+    do {
+        let res = try checkSmth()
+        print(res)
+    } catch CustomError.custom1 {
+        print("Error 1")
+    } catch {
+        print("All other possible errors")
+    }
+}
