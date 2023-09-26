6ad8bc0423064198a6de4772aa2fb12792c59585
Thore Brehmer <thoreservice@icloud.com>
2023-09-26 20:03:49 +0200

    add: accessControl.swift

--- AccessControl.swift
+++ AccessControl.swift
@@ -6,3 +6,17 @@
 //
 
 import Foundation
+
+///----------ACCESS-CONTROLL
+struct accessControl{
+    //for VARS AND FUNCS
+    private var onlyThisStruct: Int //
+    private(set) var onlyThisStructCanSet: Int //get only
+    fileprivate var onlyThisFile: Int //only for the file
+    public var everywhere: Int //default
+    
+    //static -> callable without instant of struct
+    static let callableWithoutInstance: Int = 5 //constant global
+    static func staticFunc(){} //constant global
+    private static let constValue = 5 //constant
+}
