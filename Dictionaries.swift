9ddf5b791ff9838f8156abad7b7a34288531b905
Thore Brehmer <thoreservice@icloud.com>
2023-09-26 20:01:54 +0200

    add: dictionaries.swift

--- Dictionaries.swift
+++ Dictionaries.swift
@@ -6,3 +6,28 @@
 //
 
 import Foundation
+
+///----------DICTIONARIES
+struct hashm{
+    //declar
+    var hashm: [AnyHashable : Any] = [
+        "key1": 1,
+        "1" : 2,
+        2.0 : "test"
+    ]
+    
+    //get keys
+    var listOfKeys = hashm.keys
+    //get values
+    var listOfValues = hashm.values
+    //zugriff / "containes" / nil if key does not exist
+    var ele = hashm["key1"]
+    //add
+    hashm["new"] = 5
+    //default if key does not exist yet
+    var map: [String: Int] = [:]
+    map["new",default: 0] += 1
+    //remove
+    hashm.removeValue(forKey: "new")
+    hashm.removeAll()
+}
